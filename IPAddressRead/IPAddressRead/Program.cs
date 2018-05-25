f9ed141e24008552ed103375ed4912dc32d288c6
Kishan Choudhary <kishancool11@gmail.com>
2018-05-25 11:48:38 +0530

    Intial Checkin

---
+++ IPAddressRead/IPAddressRead/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPAddressRead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var domainList = new[] { "www.google.com", "www.microsoft.com", "docs.microsoft.com", "stackoverflow.com" };
+
+            foreach (var domain in domainList)
+            {
+                var ipAddress = Dns.GetHostAddresses(domain);
+                Console.WriteLine(ipAddress[0]);
+
+                IPEndPoint remoteEP = null;
+                HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create($"https://{domain}");
+                req.ServicePoint.BindIPEndPointDelegate = delegate (ServicePoint servicePoint, IPEndPoint remoteEndPoint, int retryCount) {
+                    remoteEP = remoteEndPoint;
+                    return null;
+                };
+                req.GetResponse();
+                Console.WriteLine(remoteEP.Address.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
